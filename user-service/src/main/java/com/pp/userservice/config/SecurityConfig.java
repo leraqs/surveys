5e46e152dc059e1d4a6fbba56378c811ee170379
Lera <Lera.ses.ko.va@gmail.com>
2024-06-07 16:26:57 +0300

    ldap

--- user-service/src/main/java/com/pp/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/pp/userservice/config/SecurityConfig.java
@@ -4,7 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
+import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
+import org.springframework.security.ldap.userdetails.LdapUserDetails;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsMapper;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsService;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,6 +25,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorize -> authorize
                         .pathMatchers("api/v1/users/register").permitAll()
                         .anyExchange().authenticated()
@@ -22,5 +33,27 @@ public class SecurityConfig {
                 .httpBasic(withDefaults())
                 .formLogin(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public DefaultSpringSecurityContextSource contextSource() {
+        return new DefaultSpringSecurityContextSource("ldap://localhost:8389/dc=springframework,dc=org");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ReactiveUserDetailsService userDetailsService() {
+        return username -> {
+            LdapUserDetailsService ldapUserDetailsService = new LdapUserDetailsService(
+                    new FilterBasedLdapUserSearch("", "(uid={0})", contextSource())
+            );
+            ldapUserDetailsService.setUserDetailsMapper(new LdapUserDetailsMapper());
+            UserDetails userDetails = ldapUserDetailsService.loadUserByUsername(username);
+            return Mono.just((LdapUserDetails) userDetails);
+        };
     }
 }
