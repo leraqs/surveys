2180f518e3ce343ddf41355433e212163ede0a20
Lera <Lera.ses.ko.va@gmail.com>
2024-06-20 16:25:45 +0300

    tg bot /start command

---
+++ notification-service/src/main/java/com/pp/notificationservice/bot/Initializer.java
@@ -0,0 +1,28 @@
+package com.pp.notificationservice.bot;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class Initializer {
+
+    private final TgBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() {
+        try {
+            TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
---
+++ notification-service/src/main/java/com/pp/notificationservice/bot/TgBot.java
@@ -0,0 +1,61 @@
+package com.pp.notificationservice.bot;
+
+import com.pp.notificationservice.config.BotConfig;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class TgBot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(@NotNull Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            String username = update.getMessage().getFrom().getFirstName();
+
+            if (messageText.equals("/start")) {
+                startBot(chatId, username);
+            } else {
+                log.warn("Unexpected message");
+            }
+        }
+    }
+
+    private void startBot(long chatId, String username) {
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText("Hello, " + username);
+
+        try {
+            execute(message);
+            log.info("Reply sent");
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+}
+
