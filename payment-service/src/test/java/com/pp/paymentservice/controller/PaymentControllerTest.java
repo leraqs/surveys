3bb18a87917ae53524ca604842a16c762ae5151e
Lera <Lera.ses.ko.va@gmail.com>
2024-06-25 20:51:29 +0300

    test container for payment service test

--- payment-service/src/test/java/com/pp/paymentservice/controller/PaymentControllerTest.java
+++ payment-service/src/test/java/com/pp/paymentservice/controller/PaymentControllerTest.java
@@ -1,37 +1,58 @@
 package com.pp.paymentservice.controller;
 
 import com.braintreegateway.BraintreeGateway;
-import com.braintreegateway.PaymentMethodNonce;
-import com.braintreegateway.Result;
-import com.pp.paymentservice.config.PayPalConfig;
 import com.pp.paymentservice.model.Payment;
 import com.pp.paymentservice.repository.PaymentRepository;
 import com.pp.paymentservice.service.PaymentService;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.OracleContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@Import(PayPalConfig.class)
+@Testcontainers
 class PaymentControllerTest {
 
+    @Container
+    static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+            .withUsername("test")
+            .withPassword("test")
+            .withDatabaseName("testdb");
+
     @MockBean
-    PaymentRepository paymentRepository;
+    private PaymentRepository paymentRepository;
+
     @Autowired
-    BraintreeGateway gateway;
+    private BraintreeGateway gateway;
+
     @Autowired
     private PaymentService paymentService;
+
+    @DynamicPropertySource
+    static void registerOracleProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", oracleContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", oracleContainer::getUsername);
+        registry.add("spring.datasource.password", oracleContainer::getPassword);
+    }
+
+    @BeforeAll
+    static void setUp() {
+        oracleContainer.start();
+    }
 
     @Test
     public void test_successful_payment_returns_correct_response() {
@@ -44,7 +65,7 @@ class PaymentControllerTest {
 
         when(paymentRepository.save(any())).thenReturn(expectedPayment);
 
-        var payment = paymentService.makePayment(orderId, totalCost, nonce);
+        Optional<Payment> payment = paymentService.makePayment(orderId, totalCost, nonce);
 
         assertFalse(payment.isEmpty());
         assertEquals(orderId, payment.get().getOrderId());
